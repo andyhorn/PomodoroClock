efbec6c37eddcb4697abf640286b5f155b52c1a6
= <=>
2019-04-03 08:33:20 -0700

    Included and activated Helmet protections

--- client/app.js
+++ client/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const helmet = require('helmet');
+
+app.use(helmet({
+    frameguard: {
+      action: 'deny'
+    },
+    hidePoweredBy: {},
+    hsts: {
+      maxAge: 90 * 24 * 60 * 60 * 1000,
+      force: true
+    },
+    ieNoOpen: {},
+    noSniff: {},
+    xssFilter: {}
+}));
+
 require('dotenv').config();
 
 app.use(cors());
